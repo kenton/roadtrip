cd75812e237006896274089ed27adb18c6be2889
Kenton Newby <kentonnewby@gmail.com>
2013-02-05 14:21:51 -0500

    moved config from support/vcr.rb to spec_helper.rb

--- lib/roadtrip/trip.rb
+++ lib/roadtrip/trip.rb
@@ -1,53 +1,52 @@
 require 'forwardable'
 require 'httparty'
-require_relative '../../spec/support/vcr'
 
 module RoadTrip
   class Trip
-		extend Forwardable
+    extend Forwardable
     include HTTParty
     format :json
 
     attr_accessor :from, :to, :cost_per_gallon, :vehicle, :directions
-		def_delegator :@vehicle, :make, :vehicle_make
-		def_delegator :@vehicle, :name, :vehicle_model
+    def_delegator :@vehicle, :make, :vehicle_make
+    def_delegator :@vehicle, :name, :vehicle_model
 
     def initialize(params)
       @from            = params[:from]
       @to              = params[:to]
       @cost_per_gallon = params[:cost_per_gallon]
       @vehicle         = params[:vehicle]
-			@directions      = get_directions
-		end
+      @directions      = get_directions
+    end
 
-		def directions
-			@directions ||= get_directions
-		end
+    def directions
+      @directions ||= get_directions
+    end
 
-		def get_directions
+    def get_directions
       Trip.get('http://maps.google.com/maps/api/directions/json?',
                :query => {
                  :origin      => self.from,
                  :destination => self.to,
                  :sensor      => "false"
               })
-		end
+    end
 
-		def distance_in_miles
-			@directions["routes"][0]["legs"][0]["distance"]["text"].split(" ").first.to_f
-		end
+    def distance_in_miles
+      @directions["routes"][0]["legs"][0]["distance"]["text"].split(" ").first.to_f
+    end
 
-		def duration
-			@directions["routes"][0]["legs"][0]["duration"]["text"]
-		end
+    def duration
+      @directions["routes"][0]["legs"][0]["duration"]["text"]
+    end
 
-		# other info available for a trip
-		#   bounds - NE and SW latlng's
-		#   distance
-		#   duration
-		#   starting point latlng
-		#   ending point latlng
-		#   waypoints
+    # other info available for a trip
+    #   bounds - NE and SW latlng's
+    #   distance
+    #   duration
+    #   starting point latlng
+    #   ending point latlng
+    #   waypoints
 
-	end
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,14 @@
 require 'simplecov'
 SimpleCov.start
+
+require 'vcr'
+
+VCR.configure do |c|
+  c.cassette_library_dir = File.join("spec", "vcr")
+  c.hook_into :fakeweb
+  c.configure_rspec_metadata!
+end
+
+RSpec.configure do |c|
+  c.treat_symbols_as_metadata_keys_with_true_values = true
+end
